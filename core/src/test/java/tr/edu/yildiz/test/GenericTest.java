c57553bd43024670cd69f8e9afa69e72393170f7
Omer Taskin <otaskin@ebay.com>
2015-02-11 01:58:29 +0200

    init commit

---
+++ core/src/test/java/tr/edu/yildiz/test/GenericTest.java
@@ -0,0 +1,11 @@
+package tr.edu.yildiz.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: otaskin
+ * Date: 2/11/15
+ * Time: 1:41 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GenericTest {
+}
